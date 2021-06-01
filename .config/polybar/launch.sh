ce75557b753c83e0d29014086e66bd3e1062c7d5
badtz13 <badtzmaru139@gmail.com>
2021-05-31 21:05:30 -0700

    update launch scripts

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,5 +11,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload one &
 done
-
-echo "Polybar launched..."
