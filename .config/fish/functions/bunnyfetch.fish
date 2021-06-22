4d1470b0f7dc750d09eb3d4538603e178a1a93a8
badtz13 <badtzmaru139@gmail.com>
2021-06-21 19:14:29 -0700

    update to navy theme

--- .config/fish/functions/bunnyfetch.fish
+++ .config/fish/functions/bunnyfetch.fish
@@ -5,10 +5,11 @@ function bunnyfetch
     set kernal (uname -r | cut -d '-' -f1)
     set resolution (xrandr --listmonitors | grep / | awk  -F'[ /x]' '{sum += $4} END {printf sum"x"$6" "}')
     set shellname (basename $SHELL)
-
+    echo ""
     echo "             "(set_color red)  (set_color normal) $wmname
     echo "    (\ /)    "(set_color yellow)  (set_color normal) $kernal
     echo "    ( . .)   "(set_color green)  (set_color normal) $shellname
     echo "    c("(set_color red)'"'(set_color normal)")("(set_color red)'"'(set_color normal)")  "(set_color magenta)  (set_color normal) $resolution
     echo "             "(set_color blue)  (set_color normal) $packages
+    echo ""
 end
