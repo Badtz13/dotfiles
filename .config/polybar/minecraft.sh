327b0057c880874da222b5074688ca0602cd8f22
badtz13 <badtzmaru139@gmail.com>
2021-07-24 19:45:28 -0700

    update polybar, move scrots dir

---
+++ .config/polybar/minecraft.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo $(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/flowerland.modded.fun) | jq '.players.online')/$(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/flowerland.modded.fun) | jq '.players.max')
