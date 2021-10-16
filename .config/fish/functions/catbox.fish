597ad90c06691cc2baff71c2095d195d37d7edba
Badtz <badtzmaru139@gmail.com>
2021-10-15 17:41:30 -0700

    update fish functions, create scripts folder

--- .config/fish/functions/catbox.fish
+++ .config/fish/functions/catbox.fish
@@ -1,3 +1,3 @@
-function catbox --wraps='sh /home/badtz/.config/fish/functions/catbox.sh' --description 'alias catbox sh /home/badtz/.config/fish/functions/catbox.sh'
-  sh /home/badtz/.config/fish/functions/catbox.sh $argv; 
+function catbox --wraps='sh catbox.sh' --description 'alias catbox'
+  sh catbox.sh $argv; 
 end
--- .config/fish/functions/grab.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function grab --wraps='nhentai -o /mnt/Sync/comics/ -C --save-download-history --no-hl  --id ' --wraps='nhentai -o /mnt/Sync/comics/ -C --save-download-history --no-html --id ' --description 'alias grab nhentai -o /mnt/Sync/comics/ -C --save-download-history --no-html --id '
-  nhentai -o /mnt/Sync/comics/ -C --save-download-history --no-html --id  $argv; 
-end
