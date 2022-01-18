e6c92eda765b7858e9f8038aedcdd52b186d95b9
Badtz <badtzmaru139@gmail.com>
2022-01-18 10:27:58 -0800

    update polybar

--- .config/polybar/minecraft.sh
+++ .config/polybar/minecraft.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/flowerland.modded.fun) | jq '.players.online')/8
+echo $(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/50.47.149.76:25565) | jq '.players.online')/20
