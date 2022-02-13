8d0e69575f32f2716bd181f75e021962527c51e3
Badtz <badtzmaru139@gmail.com>
2022-02-13 12:17:07 -0800

    fix minecraft online indicators

--- .config/polybar/minecraft.sh
+++ .config/polybar/minecraft.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/50.47.149.76:25565) | jq '.players.online')/20
+echo $(echo $(curl --connect-timeout 1 -fsm 3 https://api.mcsrvstat.us/2/$1) | jq '.players.online')/$2
